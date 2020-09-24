edcf3b0998f231eb85005b292b37a3db1a344875
Malek Smida <msmida96@gmail.com>
2020-09-24 21:39:39 +0100

    #Add: AddPost component

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./index.scss";
 import StoryReel from "./StoryReel";
+import AddPost from "./AddPost";
+
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
+      <AddPost />
     </div>
   );
 }
