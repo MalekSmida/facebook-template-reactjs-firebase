e2674b62701dcefcebdf4a759b9ca611ada8553e
Malek Smida <msmida96@gmail.com>
2020-09-24 11:55:20 +0100

    @initProject

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>header</h1>
+    </div>
+  );
+}
+
+export default Header;
