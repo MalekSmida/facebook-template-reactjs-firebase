3e3385ac49af5c369028c75df9c537a54fe33738
Malek Smida <msmida96@gmail.com>
2020-09-25 16:41:49 +0100

    #Edit: login style

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -21,16 +21,10 @@ function Login() {
   };
   return (
     <div className="login">
-      <div className="login__logo">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
-          alt="f"
-        />
-        <img
-          src="https://logos-world.net/wp-content/uploads/2020/04/Facebook-Logo.png"
-          alt="facebook"
-        />
-      </div>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+        alt="f"
+      />
       <Button type="submit" onClick={signIn}>
         Sign in
       </Button>
